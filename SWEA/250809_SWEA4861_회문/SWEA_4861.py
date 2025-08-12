c550f9b6db2494f1d03d38183560fdd94d12bd56
김영훈 <yhkor11@naver.com>
2025-08-12 18:06:28 +0900

    modify file 회문

--- SWEA/250809_SWEA4861_회문/SWEA_4861.py
+++ SWEA/250809_SWEA4861_회문/SWEA_4861.py
@@ -1,22 +1,55 @@
-Test = int(input())
-for test in range(1 , Test+1):
-    N, M = map(int, input().split())
-    str_List = [list(map(str, input().split())) for _ in range(N)]
-    for i in range(N-M+1):
-        for j in range(N-M+1):
-            LR_str = ''
-            L_str = ''
-            R_str = ''
-            for k in range(M//2): # m//2 만큼 양쪽에서 가운데까지 이동
-                if((j+k) < (M+j-1-k) and str_List[i][j+k] == str_List[i][M+j-1-k]): 
-                    #  왼쪽 < 오른쪽 이고 각 인덱스의 문자가 같으면
-                    # if(str_List[i][j+k] == str_List[i][M+j-1-k]): 
-                    L_str += str_List[i][j+k]
-                    R_str += str_List[i][M+j-1-k]
-                    print(L_str, R_str, "L, R str 임")
-                
-                elif(str_List[i][j+k] != str_List[i][M+j-1-k]): # 다르면
-                    break
-            # if(L_str == R_str): # 왼쪽 오른쪽 같으면
-            LR_str = L_str+R_str
-            print(LR_str)
+test = int(input())
+for t in range(1, test+1):
+    n, m = map(int, input().split())
+    str_list = [list(map(str, input().split())) for _ in range(n)]
+    for i in range(n):
+        # Lr_str = ''
+        l_str = ''
+        r_str = ''
+        sum_str
+        print('첫 포문')
+        for j in range(n-m+1):
+            print('2 포문')
+
+            if (j+m) < n:
+                print('격자크기 확인')
+
+                for k in range((m-1)//2):  # m//2 만큼 양쪽에서 가운데까지 이동
+                    if()
+
+                    l_str += str_list[i][j+k]
+                    r_str += str_list[i][j+m-k]
+                    print(l_str, r_str, '홀수 ㅇ;ㄹ때ㅔ')
+
+                    elif( != )
+
+
+            if l_str == r_str and m%2 == 0:
+                # print(l_str+r_str)
+                sum_str = l_str+r_str
+            if l_str == r_str and m%2 == 1:
+                # print(l_str + str_list[i][j] + r_str)
+                sum_str = l_str+str_list[i][j]+r_str
+    print(sum_str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ # if m % 2 == 0:
+                #     for l in range(m // 2):  # m//2 만큼 양쪽에서 가운데까지 이동
+                #
+                #         l_str += str_list[i][j+l]
+                #         r_str += str_list[i][j+m-l]
+                #         print(l_str, r_str, '짝수일때')
