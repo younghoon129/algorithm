70ba16f4e1c636e00c673ea1f0c19e0af5560199
김영훈 <yhkor11@naver.com>
2025-08-12 20:04:37 +0900

    modify file 회문

--- SWEA/250809_SWEA4861_회문/SWEA_4861.py
+++ SWEA/250809_SWEA4861_회문/SWEA_4861.py
@@ -4,32 +4,34 @@ for t in range(1, test+1):
     str_list = [list(map(str, input().split())) for _ in range(n)]
     for i in range(n):
         # Lr_str = ''
-        l_str = ''
-        r_str = ''
-        sum_str
         print('첫 포문')
         for j in range(n-m+1):
             print('2 포문')
-
-            if (j+m) < n:
+            l_str = ''
+            r_str = ''
+            sum_str = ''
+            print(j+m, n)
+            print(j)
+            if (j+m-1) <= n-1 and str_list[i][j] == str_list[i][j+m-1]:
                 print('격자크기 확인')
 
-                for k in range((m-1)//2):  # m//2 만큼 양쪽에서 가운데까지 이동
-                    if()
-
+                for k in range(m//2):  # m//2 만큼 양쪽에서 가운데까지 이동
+                    
                     l_str += str_list[i][j+k]
                     r_str += str_list[i][j+m-k]
-                    print(l_str, r_str, '홀수 ㅇ;ㄹ때ㅔ')
+                    
+                    if 
 
-                    elif( != )
+                    elif(str_list[i][j+k] != str_list[i][j+m-k]):
+                        break                        
 
-
-            if l_str == r_str and m%2 == 0:
-                # print(l_str+r_str)
-                sum_str = l_str+r_str
-            if l_str == r_str and m%2 == 1:
-                # print(l_str + str_list[i][j] + r_str)
-                sum_str = l_str+str_list[i][j]+r_str
+                else:
+                    if l_str == r_str and m%2 == 0:
+                        # print(l_str+r_str)
+                        sum_str = l_str+r_str
+                    if l_str == r_str and m%2 == 1:
+                        # print(l_str + str_list[i][j] + r_str)
+                        sum_str = l_str+str_list[i][j]+r_str
     print(sum_str)
 
 
