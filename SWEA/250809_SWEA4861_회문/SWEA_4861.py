a2af5927f5730818c38463ed9a542e834bc2b478
김영훈 <yhkor11@naver.com>
2025-08-14 16:51:23 +0900

    ífile upload 회문

--- SWEA/250809_SWEA4861_회문/SWEA_4861.py
+++ SWEA/250809_SWEA4861_회문/SWEA_4861.py
@@ -1,38 +1,35 @@
 test = int(input())
 for t in range(1, test+1):
     n, m = map(int, input().split())
-    str_list = [list(map(str, input().split())) for _ in range(n)]
+    str_list = [list(map(str, input())) for _ in range(n)]
     for i in range(n):
-        # Lr_str = ''
-        print('첫 포문')
         for j in range(n-m+1):
-            print('2 포문')
             l_str = ''
             r_str = ''
             sum_str = ''
-            print(j+m, n)
-            print(j)
-            if (j+m-1) <= n-1 and str_list[i][j] == str_list[i][j+m-1]:
-                print('격자크기 확인')
-
-                for k in range(m//2):  # m//2 만큼 양쪽에서 가운데까지 이동
-                    
-                    l_str += str_list[i][j+k]
-                    r_str += str_list[i][j+m-k]
-                    
-                    if 
-
-                    elif(str_list[i][j+k] != str_list[i][j+m-k]):
-                        break                        
-
-                else:
-                    if l_str == r_str and m%2 == 0:
-                        # print(l_str+r_str)
-                        sum_str = l_str+r_str
-                    if l_str == r_str and m%2 == 1:
-                        # print(l_str + str_list[i][j] + r_str)
-                        sum_str = l_str+str_list[i][j]+r_str
-    print(sum_str)
+            print(n, m, j, j+m)
+            if (i+m-1) < n and (j+m-1) < n:
+                if str_list[i][j] == str_list[i][j+m-1]: # (j+m) = 19, n = 20
+                    # 격자 안에 있고, 첫글자 끝 글자 같다면
+                    print("if문 들왔음")
+                    for k in range((m//2)-1):  # m//2 만큼 양쪽에서 가운데까지 이동
+                        print('k 들어 왓음')
+                        if str_list[i][j+k] == str_list[i][j+m-k-1]:
+                            print('k 안에 if 들어왓음')
+                            l_str += str_list[i][j+k]
+                            r_str += str_list[i][j+m-k]
+                        else:
+                            print('k 안에 else')
+                            break
+                    else:
+                        print('for else 썻음 for문 정상 끝나면 실행')
+                        if l_str == r_str and m % 2 == 1:
+                            print('for else 안에 if')
+                            # print(l_str + str_list[i][j] + r_str)
+                            sum_str = l_str+str_list[i][j+(m//2)]+r_str
+                        # elif(str_list[i][j+k] != str_list[i][j+m-k]):
+                        #     break
+        print(sum_str)
 
 
 
