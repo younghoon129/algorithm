ff7679f98c6a0bdb4ab5ff8ecf29c13dc5d72d35
김영훈 <yhkor11@naver.com>
2025-08-16 17:31:37 +0900

    modified files,folders SWEA

---
+++ SWEA/0_IM복기/250816_SWEA5202_화물도크/SWEA_5202.py
@@ -0,0 +1,33 @@
+# # 12:50 ~ 14:10  포기
+# test = int(input())
+
+# for t in range(1, test+1):
+#     num = int(input())
+#     time_se = []
+#     e = []
+#     # time_fl = []
+#     for n in range(num):
+#         s, e = map(int, input().split())
+#         time_se.append([s, e])  # time
+#         e.append(e)
+#         e.sort()
+#     # print(time)
+#     for i in time_se:  # 각 배열 안의 시작, 끝 = i
+
+#         for start, end in i:
+#             # 배열의 첫 시작
+#             for 시작 시간들 비교 range(num):
+                
+
+
+
+
+
+
+
+    # for time_se in time:
+    #     cnt = 0  # 시간 길이
+    #     for time_length in range(time_se[0], time_se[1]):
+    #         cnt += 1
+    #     time_fl.append(cnt)
+    # print(time_fl)
