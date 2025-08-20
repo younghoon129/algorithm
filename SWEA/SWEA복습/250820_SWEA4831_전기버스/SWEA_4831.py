0ac3afc117ef582e3c81088cce93597c89e13d13
김영훈 <yhkor11@naver.com>
2025-08-21 01:17:33 +0900

    file upload 전기버스

--- SWEA/SWEA복습/250820_SWEA4831_전기버스/SWEA_4831.py
+++ SWEA/SWEA복습/250820_SWEA4831_전기버스/SWEA_4831.py
@@ -1,34 +1,33 @@
 # 9시 18분 시작
-#48분 정답보기
+#48분 
+# 12:51분 정답보기
+# 12:59분 다시 시작
+# 1:16분
+# import sys
+# sys.stdin = open("4828_input.txt", "r")
+
 test = int(input())
 
 for t in range(1, test+1):
     k, n, m = map(int,input().split())
-    bus_st = list(map(int, input().split()))
+    bus_st = set(map(int, input().split()))
     charge = [0]*(n+1)
     cnt = 0
     for c in bus_st:
         charge[c] = c
-
-    # for i in range(len(charge)):
-    #     if charge[i+k] != 0:
-    #         cnt += 1
-    #     elif charge[:i+k] != 0:
-    #         cnt += 1
     i = 0
-    while True:
-        # if i+k <= n:
-        if n-m+1<= i:
+    while i+k < n:
+        nc = i + k
+        
+        while nc > i and nc not in bus_st:
+            nc-=1
+        if nc == i:
+            cnt = 0
             break
-        if charge[i+k] != 0:
-            cnt += 1
-            i = k
-        elif charge[i+k] == 0:
-            for j in range(i+k, i, -1):
-                if j != 0:
-                    i = j
-                    cnt += 1
-        print(cnt)
+        i = nc
+        # print(i, 'dijasoidjasoi')
+        cnt += 1
+    print(f"#{t} {cnt}")
         
 
 
