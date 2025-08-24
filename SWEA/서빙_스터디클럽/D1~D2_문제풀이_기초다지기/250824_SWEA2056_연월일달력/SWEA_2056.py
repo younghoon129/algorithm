f312a1cf4a78c1a906ddbe918997412bc21a7eed
김영훈 <yhkor11@naver.com>
2025-08-24 23:39:50 +0900

    modified file _ 연월일달력

--- SWEA/서빙_스터디클럽/D1~D2_문제풀이_기초다지기/250824_SWEA2056_연월일달력/SWEA_2056.py
+++ SWEA/서빙_스터디클럽/D1~D2_문제풀이_기초다지기/250824_SWEA2056_연월일달력/SWEA_2056.py
@@ -1,6 +1,29 @@
-# 22: 46 ~ 22 : 46
+# 23: 04 ~ 23 : 38
 test = int(input())
-
+cal = {
+    '01' : 31,
+    '02' : 28,
+    '03' : 31,
+    '04' : 30,
+    '05' : 31,
+    '06' : 30,
+    '07' : 31,
+    '08' : 31,
+    '09' : 30,
+    '10' : 31,
+    '11' : 30,
+    '12' : 31
+    }
 for t in range(1, test+1):
-    n = list(map(int, input().split()))
-    print(f"#{t} {max(n)}")
+    n = input()
+    box = [0]*3
+    box[0] = n[0:4]
+    box[1] = n[4:6]
+    box[2] = n[6:]
+    if 1 <= int(box[1]) <= 12:
+        if 1 <= int(box[2]) <= cal[box[1]]:
+            print(f"#{t} {'/'.join(box)}")
+        else:
+            print(f"#{t} -1")
+    else:
+        print(f"#{t} -1")
