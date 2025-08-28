3c812e1d85cc0746285df10e9bbd4505f9b75e0b
김영훈 <yhkor11@naver.com>
2025-08-28 17:54:50 +0900

    file upload - 어디에단어가들어갈수있을까, 조교의성적매기기

---
+++ SWEA/서빙_스터디클럽/D1~D2_문제풀이_기초다기지_2/250827_SWEA1983_조교의성적매기기/SWEA_1983.py
@@ -0,0 +1,10 @@
+# # 15:03 ~ 15: 09
+# import sys
+# sys.stdin = open("/input.txt", "r")
+
+# test = int(input())
+# for t in range(1, test+1):
+#     n, m = map(int, input().split())
+#     box = [list(map(int, input().split())) for _ in range(n)]
+#     max_cnt = 0
+#     for i in range(n-m+1):
