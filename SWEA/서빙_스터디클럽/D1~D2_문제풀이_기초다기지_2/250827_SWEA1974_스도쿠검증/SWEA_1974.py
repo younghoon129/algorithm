c868b7dd42561d84c9a1015c91a60ea5e4fdf064
김영훈 <yhkor11@naver.com>
2025-09-04 20:15:48 +0900

    file upload - 스도쿠검증

--- SWEA/서빙_스터디클럽/D1~D2_문제풀이_기초다기지_2/250827_SWEA1974_스도쿠검증/SWEA_1974.py
+++ SWEA/서빙_스터디클럽/D1~D2_문제풀이_기초다기지_2/250827_SWEA1974_스도쿠검증/SWEA_1974.py
@@ -5,17 +5,3 @@
 test = int(input())
 for t in range(1, test+1):
     time = list(map(int, input().split()))
-    h = 0
-    m = 0
-    for ti in range(4):
-        if ti%2 == 0:
-            h += time[ti]
-            if h >= 12:
-                h %= 12
-        else:
-            m += time[ti]
-            if m >= 60:
-                h += 1
-                m %= 60
-
-    print(f"#{t} {h} {m}")
