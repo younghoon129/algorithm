ca66cecae3430f82051490fc29ea0d6b5218c85e
김영훈 <yhkor11@naver.com>
2025-09-08 22:41:35 +0900

    file upload - 신뢰

---
+++ SWEA/18반클럽/0_IM복기/250908_SWEA10761_신뢰/SWEA_10761.py
@@ -0,0 +1,18 @@
+test = int(input())
+for t in range(1, test+1):
+    box = list(input().split())
+    b = []  # 문자
+    o = []  # 숫자
+    b_cnt = 1
+    o_cnt = 1
+    max_cnt = 1
+    m= 0
+    for i in range(int(box[0])):
+        if box[2*i] == 'B':
+            b.append([box[2*i],box[2*i+1]])
+        elif box[2*i] == 'O':
+            o.append([box[2*i],box[2*i+1]])
+    
+    for j in range()
+
+    for j in range(1, max(m)+1):
