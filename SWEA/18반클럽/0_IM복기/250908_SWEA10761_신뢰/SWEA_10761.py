a0d7de3015af9491b38867bafe7db6387f2a5c58
김영훈 <yhkor11@naver.com>
2025-09-09 21:14:18 +0900

    file upload

--- SWEA/18반클럽/0_IM복기/250908_SWEA10761_신뢰/SWEA_10761.py
+++ SWEA/18반클럽/0_IM복기/250908_SWEA10761_신뢰/SWEA_10761.py
@@ -5,14 +5,24 @@ for t in range(1, test+1):
     o = []  # 숫자
     b_cnt = 1
     o_cnt = 1
-    max_cnt = 1
-    m= 0
+    bm = 0
+    om = 0
     for i in range(int(box[0])):
-        if box[2*i] == 'B':
-            b.append([box[2*i],box[2*i+1]])
-        elif box[2*i] == 'O':
-            o.append([box[2*i],box[2*i+1]])
-    
-    for j in range()
+        if box[(2*i)+1] == 'B':
+            b.append([box[(2*i)+1],box[2*(i+1)]])
+            print([box[(2*i)+1],box[2*(i+1)]], i)
+        elif box[(2*i)+1] == 'O':
+            o.append([box[(2*i)+1],box[2*(i+1)]])
+            print([box[(2*i)+1],box[2*(i+1)]], i)
 
-    for j in range(1, max(m)+1):
+    for mb in b:
+        bm = max(bm, int(mb[1]))
+    for mo in o:
+        om = max(om, int(mo[1]))
+    mm = max(bm, om)
+    
+    
+    
+    # for j in range()
+
+    # for j in range(1, max(m)+1):
