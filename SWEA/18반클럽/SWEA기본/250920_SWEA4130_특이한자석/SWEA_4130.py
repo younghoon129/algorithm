3f6741e42f64de7f2469d88970c9a6ad27ea1114
김영훈 <yhkor11@naver.com>
2025-09-21 11:00:02 +0900

    modified file - 특이한자석

--- SWEA/18반클럽/SWEA기본/250920_SWEA4130_특이한자석/SWEA_4130.py
+++ SWEA/18반클럽/SWEA기본/250920_SWEA4130_특이한자석/SWEA_4130.py
@@ -1,36 +1,40 @@
 from collections import deque
-def turn_mag(x, y, mag_sum):  # x : 자석번호, y : 회전방향
-    global magnetic
+def turn_mag(x, y):  # x : 자석번호, y : 회전방향, 여기서 회전 해야될 정보만 만들자
+    global magnetic, result
     if x == nn:
         return
     if x < 0:
         return
     magnetic[x].rotate(y)
-    
+    print(magnetic[x])
 
     if magnetic[x][0] == 1:
-        mag_sum += mag_point[x]
+        result += mag_point[x]
+        print('더하기')
 
-    if 0 <= x-1 and x+1 < m_num:
+    if 0 <= x-1:  # 왼쪽 자석 있다면
+        print(y, '조건1')
         if magnetic[x][6] != magnetic[x-1][2]:
-            turn_mag(x-1, -y, mag_sum)
+            turn_mag(x-1, -(y))
+            print('1')
+    if x+1 < m_num:  # 오른쪽 자석 있다면
+        print(y, '조건2')
         if magnetic[x][2] != magnetic[x+1][6]:
-            turn_mag(x+1, -y, mag_sum)
+            turn_mag(x+1, -(y))
+            print('2')
 
 tc = int(input())
 for t in range(1, tc+1):
     m_num = 4
     nn = int(input())
     magnetic = ([deque(map(int, input().split())) for _ in range(m_num)])
-    mag_sum = 0
     result = 0
     mag_point = [1, 2, 4, 8]
     for n in range(nn):
         x, y = map(int, input().split())
-        turn_mag(x, y, mag_sum)
-        result += mag_sum
+        x -= 1
+        turn_mag(x, y)
     print(result)
-
 # 1
 # 2
 # 0 0 1 0 0 1 0 0
