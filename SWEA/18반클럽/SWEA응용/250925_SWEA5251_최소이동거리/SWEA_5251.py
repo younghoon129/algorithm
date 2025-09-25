1b8090e6d3f060e3e1a431c15f986241268f9c96
김영훈 <yhkor11@naver.com>
2025-09-25 23:23:56 +0900

    file upload - 최단이동거리_Dijkstra

---
+++ SWEA/18반클럽/SWEA응용/250925_SWEA5251_최소이동거리/SWEA_5251.py
@@ -0,0 +1,33 @@
+from collections import defaultdict
+import heapq, math
+
+def dijkstra(graph, start):
+    distances = {v : math.inf for v in graph}
+    distances[start] = 0
+    min_heap = []
+    heapq.heappush(min_heap, [0, start])
+
+    while min_heap:
+        c_distance, c_vertex = heapq.heappop(min_heap)
+        # print(c_distance, c_vertex, '@@@@@@@@@')
+        if distances[c_vertex] < c_distance: continue
+        for adjacent, weight in graph[c_vertex].items():
+            distance = c_distance + weight
+            if distance < distances[adjacent]:
+                distances[adjacent] = distance
+                heapq.heappush(min_heap, [distance, adjacent])
+    return distances[n]
+
+tc = int(input())
+for t in range(1, tc+1):
+    n, m = map(int, input().split())
+    box = [list(map(int, input().split())) for _ in range(m)]
+    graph = defaultdict(dict)
+    for i in range(n+1):
+        graph[i]
+    for j in range(m):
+        graph[box[j][0]].update({box[j][1] : box[j][2]})
+    # print(graph)
+    result = dijkstra(graph, 0)
+    print(f"#{t} {result}")
+
