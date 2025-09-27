ea0b69081a3aff90f6c901ca03840a7f79e0a873
김영훈 <yhkor11@naver.com>
2025-09-27 10:39:56 +0900

    modified file - min_cost

--- SWEA/18반클럽/0_IM복기/250904_SWEA16811_당근포장하기/SWEA_16811_정답.py
+++ SWEA/18반클럽/0_IM복기/250904_SWEA16811_당근포장하기/SWEA_16811_정답.py
@@ -4,37 +4,39 @@ for tc in range(1, T+1):
     n = int(input())
     nums = list(map(int, input().split()))
  
-    nums.sort()  #정렬하고
+    nums.sort()  # 정렬하고
     carrot = [0]  #
-    a = nums[0]
+    a = nums[0]  # 정렬한거에 제일 작은 값
     b = 0
-    
+
     for i in range(n):
-        if a == nums[i]:
-            carrot[-1] += 1
+        if a == nums[i]:  # 만약 a(정렬한거에 첫번째 값) == nums(정렬 배열) 순회
+            carrot[-1] += 1  # carrot 뒤부터 +1
         else:
-            carrot.append(1)
-            a = nums[i]
-             
+            carrot.append(1)  # 아니면 carrot 뒤에 1 추가
+            a = nums[i]  # a 초기화
+    #  =====================================각 숫자의 갯수 구한거 
+    
     ans = -1
-    nn = len(carrot)
+    nn = len(carrot)  # 당근배열의 갯수
     m_box = []
     s_box = []
     bad_car = n // 2
  
     for i in range(nn):  # 작은 바구니
         s_box.append(carrot[i])
-        if sum(s_box) > bad_car:
-            break
-        m_box = []
+        if sum(s_box) > bad_car:  # 만약 각 사이즈의 당근 다 더한게 기준보다 크면
+            break  # 브레이크
+        m_box = []  # 초기화
+
         for j in range(i+1, nn):  # 중간 바구니
-            m_box.append(carrot[j])
-            if sum(m_box) > bad_car:
+            m_box.append(carrot[j])  # s_box 넣은거 제외하고 추가
+            if sum(m_box) > bad_car:  # 만약 크면 브레이크
                 break  
-             
+            
             a = sum(s_box)
             b = sum(m_box)
-            c = sum(carrot[j+1:]) # 큰 바구니
+            c = sum(carrot[j+1:]) # 큰 바구니 m_box 까지 넣은 거 제외하고 추가
              
             if a == 0 or b == 0 or c == 0 or c > bad_car:  # 비어있는 상자가 있으면 pass
                 continue   
--- SWEA/18반클럽/SWEA응용/250925_SWEA5250_최소비용/SWEA_5250.py
+++ SWEA/18반클럽/SWEA응용/250925_SWEA5250_최소비용/SWEA_5250.py
@@ -1,54 +1,34 @@
 import sys
-sys.stdin = open('C:\\Users\\user\\Desktop\\git\\algorism\\SWEA\\18반클럽\\SWEA응용\\250925_SWEA5250_최소비용\\SWEA_5250_input.txt')
+sys.stdin = open('C:\\Users\\user\\Desktop\\코딩폴더 깃허브\\김영훈\\algorithm\\SWEA\\18반클럽\\SWEA응용\\250925_SWEA5250_최소비용\\SWEA_5250_input.txt')
 dxy = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
 
 import heapq
 
 
-def dijkstra(bl, i, j):
+def dijkstra(height, i, j):
     global result
-    visited = [[float('INF')]*n for _ in range(n)]
+    cost = [[float('INF')]*n for _ in range(n)]
     queue = []
     heapq.heappush(queue, (i, j))
-    visited[i][j] = 0
+    cost[i][j] = 0
     while queue:
         x, y = heapq.heappop(queue)
-        if x == n-1 and y == n-1:
-            return visited[n-1][n-1]
+        
         for dx, dy in dxy:
             nx, ny = dx + x, dy + y
             if 0 <= nx < n and 0 <= ny < n:
-                if bl[x][y] < bl[nx][ny]:
-                    if visited[x][y] + (bl[nx][ny]-bl[x][y]+1) < visited[nx][ny]:
-                        visited[nx][ny] = visited[x][y] +(bl[nx][ny] - bl[x][y] + 1)
+                n_cost = cost[x][y]+(height[nx][ny]-height[x][y]+1)
+                if height[x][y] < height[nx][ny]:
+                    if n_cost < cost[nx][ny]:
+                        cost[nx][ny] = n_cost
                         heapq.heappush(queue, (nx, ny))
-                elif visited[x][y] + (bl[nx][ny]-bl[x][y]+1) < visited[nx][ny]:
-                    visited[nx][ny] = visited[x][y] + 1
-                    heapq.heappush(queue, (nx, ny))
-        
-    return visited[n-1][n-1]        
-
-    # while queue:
-    #     cost, x, y = heapq.heappop(queue)
- 
-    #     if cost > visited[x][y]:  # 이동 비용이 저장된 비용보다 크면 pass
-    #         continue
- 
-    #     for dx, dy in dxy:
-    #         nx, ny = x + dx, y + dy
-    #         if 0 <= nx < n and 0 <= ny < n:
-    #             next_cost = 1  # 다음 칸 기본 이동 비용 1
-    #             if bl[nx][ny] > bl[x][y]:  # 다음칸 높이가 더 높으면
-    #                 next_cost += (bl[nx][ny] - bl[x][y])  # 이동 비용에다 높이 차이만큼 +
-    #             new_dist = cost + next_cost  # 새로운 이동 비용 = 현재까지의 비용 + 이동 비용
- 
-    #             if new_dist < visited[nx][ny]:  # 새로운 이동 비용이 기록된 최소 비용보다 적으면 갱신
-    #                 visited[nx][ny] = new_dist
-    #                 heapq.heappush(queue, (new_dist, nx, ny))
- 
-    #     if (x, y) == (n - 1, n - 1):  # 도착하면 반환
-    #         return cost  
+                if height[x][y] >= height[nx][ny]:
+                    if cost[x][y]+1 < cost[nx][ny]:
+                        cost[nx][ny] = cost[x][y] + 1
+                        heapq.heappush(queue, (nx, ny))
+        if x == n-1 and y == n-1:
+            return cost[n-1][n-1]
 
 tc = int(input())
 
