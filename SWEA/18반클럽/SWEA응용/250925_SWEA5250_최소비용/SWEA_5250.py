0eae09830c43dead3bb2ff49cfca5bc5c2b602f5
김영훈 <yhkor11@naver.com>
2025-09-29 13:17:21 +0900

    modified file - MinCost

--- SWEA/18반클럽/SWEA응용/250925_SWEA5250_최소비용/SWEA_5250.py
+++ SWEA/18반클럽/SWEA응용/250925_SWEA5250_최소비용/SWEA_5250.py
@@ -1,46 +1,28 @@
-import sys
-sys.stdin = open('C:\\Users\\user\\Desktop\\코딩폴더 깃허브\\김영훈\\algorithm\\SWEA\\18반클럽\\SWEA응용\\250925_SWEA5250_최소비용\\SWEA_5250_input.txt')
-dxy = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+# dxy = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 
+# from collections import deque
+# import heapq
+# def dijkstra(box, x, y, cnt):
+#     queue = []
+#     heapq.heappush(queue, (x, y))
+#     while queue:
+#         x, y = heapq.heappop(queue)
+#         visited[x][y] = True
 
-import heapq
+#         # if visited[x][y]:
+#         # 힙큐로 경로를 가장 낮은 값들로 하고 빈 set에 if not in set 해서
+#         # 세트 안에 없으면 추가 하고 그것들 카운트해서 cnt에 값 더하기
+#         for dx, dy in dxy:
+#             nx = dx + x
+#             ny = dy + y
+#             if 0 <= nx < n and 0 <= ny < n and not visited[nx][ny]:
 
+#     pass
 
-def dijkstra(height, i, j):
-    global result
-    cost = [[float('INF')]*n for _ in range(n)]
-    queue = []
-    heapq.heappush(queue, (i, j))
-    cost[i][j] = 0
-    while queue:
-        x, y = heapq.heappop(queue)
-        
-        for dx, dy in dxy:
-            nx, ny = dx + x, dy + y
-            if 0 <= nx < n and 0 <= ny < n:
-                n_cost = cost[x][y]+(height[nx][ny]-height[x][y]+1)
-                if height[x][y] < height[nx][ny]:
-                    if n_cost < cost[nx][ny]:
-                        cost[nx][ny] = n_cost
-                        heapq.heappush(queue, (nx, ny))
-                if height[x][y] >= height[nx][ny]:
-                    if cost[x][y]+1 < cost[nx][ny]:
-                        cost[nx][ny] = cost[x][y] + 1
-                        heapq.heappush(queue, (nx, ny))
-        if x == n-1 and y == n-1:
-            return cost[n-1][n-1]
-
-tc = int(input())
-
-for t in range(1, tc+1):
-    n = int(input())
-    box = [list(map(int, input().split())) for _ in range(n)]
-    result = dijkstra(box, 0, 0)
-    print(f"#{t} {result}")
-
-
-# 1
-# 3
-# 0 2 1
-# 0 1 1
-# 1 1 1
+# tc = int(input())
+# for t in range(1, tc+1):
+#     n = int(input())
+#     box = [list(map(int, input().split())) for _ in range(n)]
+#     cnt = 0
+#     visited = [[0] for _ in range(n)]
+#     result = dijkstra(box, 0, 0, cnt)
