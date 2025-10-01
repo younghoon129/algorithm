232311db8d405203814d5694490aedd4ceea7d17
김영훈 <yhkor11@naver.com>
2025-10-01 21:24:55 +0900

    modified file - quicksort_success

--- SWEA/18반클럽/SWEA응용/251001_SWEA5205_quicksort/SWEA_5205.py
+++ SWEA/18반클럽/SWEA응용/251001_SWEA5205_quicksort/SWEA_5205.py
@@ -1,1 +1,31 @@
-#quick
+def quick_sort(arr, start, end):
+    if start < end:
+        p = partition(arr, start, end)
+
+        quick_sort(arr, start, p - 1)
+        quick_sort(arr, p + 1, end)
+
+def partition(arr, start, end):
+    p = arr[start]
+    left = start + 1
+    right = end
+
+    while True:
+        while left <= end and arr[left] < p:
+            left += 1
+        while right > start and arr[right] >= p:
+            right -= 1
+        if left < right:
+            arr[left], arr[right] = arr[right], arr[left]
+        else:
+            break
+    arr[start], arr[right] = arr[right], arr[start]
+    return right
+
+
+tc = int(input())
+for t in range(1, tc+1):
+    n = int(input())
+    box = list(map(int, input().split()))
+    box.sort()
+    print(f"#{t} {box[n//2]}")
