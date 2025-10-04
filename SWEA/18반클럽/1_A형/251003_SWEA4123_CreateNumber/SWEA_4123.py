563e9fd3159dd7553ddf3f3256686ea02f7736a4
김영훈 <yhkor11@naver.com>
2025-10-04 23:12:13 +0900

    modified file - CreateNum

--- SWEA/18반클럽/1_A형/251003_SWEA4123_CreateNumber/SWEA_4123.py
+++ SWEA/18반클럽/1_A형/251003_SWEA4123_CreateNumber/SWEA_4123.py
@@ -1,10 +1,34 @@
 # 19:31
 import sys
-sys.stdin = open('C:\\Users\\user\\Desktop\\코딩폴더 깃허브\\김영훈\\algorithm\\SWEA\\18반클럽\\1_A형\\251003_SWEA4123_MakeNumber\\SWEA_4123_input.txt', 'r')
+sys.stdin = open('C:\\Users\\user\\Desktop\\코딩폴더 깃허브\\김영훈\\algorithm\\SWEA\\18반클럽\\1_A형\\251003_SWEA4123_CreateNumber\\SWEA_4123_input.txt', 'r')
 
 
-def dfs(oper, nl):
+def dfs(op, idx, res):
+    global max_num, min_num
+
+    if idx == n:
+        max_num = max(max_num, res)
+        min_num = min(min_num, res)
+        return
     
+    for op_idx, op_cnt in enumerate(op):
+        if op_cnt == 0:
+            continue
+        tmp_res = res
+        if op_idx == 0:
+            tmp_res += num_list[idx]
+        elif op_idx == 1:
+            tmp_res -= num_list[idx]
+        elif op_idx == 2:
+            tmp_res *= num_list[idx]
+        elif op_idx == 3:
+            if num_list[idx] == 0:
+                return
+            tmp_res = int(tmp_res / num_list[idx])
+        
+        op[op_idx] -= 1
+        dfs(op, idx +1, tmp_res)
+        op[op_idx] += 1
 
 
 tc = int(input())
@@ -14,7 +38,7 @@ for t in range(1, tc+1):
     num_list = list(map(int, input().split()))
     max_num = float('-inf')
     min_num = float('inf')
-    
-    dfs(operate, num_list)
-
-# + 2, - 1, * 0, / 1
+    init_num = num_list[0]
+    init_idx = 1
+    dfs(operate, init_idx, init_num)
+    print(f"#{t} {max_num - min_num}")
