249a51ff35d68b8417ed64ea0bb7845eb6d32439
김영훈 <yhkor11@naver.com>
2025-10-06 12:20:28 +0900

    file upload - 이항계수_DP

---
+++ SWEA/18반클럽/Algorithm_coaching/DP/이항계수_DP.py
@@ -0,0 +1,14 @@
+def bino(n, k):
+    B = [[0 for _ in range(k+1)] for _ in range(n+1)]
+    for i in range(n+1):
+        for j in range(min(i, k) + 1):
+            if j == 0 or j == i:
+                B[i][j] = 1
+            else:
+                B[i][j] = B[i - 1][j - 1] + B[i -1][j]
+        
+    return B[n][k]
+
+n = 5
+k = 2
+print(bino(n, k))
