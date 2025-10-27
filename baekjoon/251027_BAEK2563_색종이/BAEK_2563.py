a28c17bbe5d4e2082f7a97c74a217e0d3a61bca0
김영훈 <yhkor11@naver.com>
2025-10-27 17:17:38 +0900

    modified file - colorpaper

--- baekjoon/251027_BAEK2563_색종이/BAEK_2563.py
+++ baekjoon/251027_BAEK2563_색종이/BAEK_2563.py
@@ -1,19 +1,25 @@
 import sys
 sys.stdin = open('BAEK_2563_input.txt', 'r')
-
+from pprint import pprint
 def put(h, y):
     global box
-    for i in range((n - 1 - h), (n - 1 - h - p_size)):
+    a = n-1-h
+    for i in range(a, a-p_size, -1):
         for j in range(y, (y + p_size)):
+            box[i][j] = 1
 
+def find_one(box):
+    global result
+    result += sum(map(sum, box))
 
-
+result = 0
 n = 100
 paper = int(input())
 p_size = 10
 box = [[0] * n for _ in range(n)]
 
-
 for i in range(paper):
     x, y = map(int, input().split())
     put(y, x)
+find_one(box)
+print(result)
