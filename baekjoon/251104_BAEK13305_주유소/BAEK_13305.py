a37b75c1d7967637d6e7a9643776df85b95818a4
pieangel11 <pieangel11@naver.com>
2025-11-04 12:06:02 +0900

    modified file - - oil

--- baekjoon/251104_BAEK13305_주유소/BAEK_13305.py
+++ baekjoon/251104_BAEK13305_주유소/BAEK_13305.py
@@ -1,48 +1,46 @@
 import sys
 sys.stdin = open('BAEK_13305_input.txt', 'r')
 
+# n = int(input())
+# road = list(map(int, input().split()))
+# oil = list(map(int, input().split()))
+# cnt = 0
+# coin = 0
+# i = 0 # 기름
+# j = 0 # 길
+# k = 1
+#
+# while j < len(road):
+#     if cnt < road[j]:
+#         cnt = road[j]
+#         coin = coin + (oil[i]*road[j])
+#     if oil[i] < oil[i+k]:
+#         k += 1
+#         cnt = 0
+#         j += 1
+#         continue
+#     if oil[i] >= oil[i+k]:
+#         cnt = 0
+#         i += k
+#         j += 1
+#         k = 1
+#         continue
+#
+# print(coin)
+
+
 n = int(input())
 road = list(map(int, input().split()))
 oil = list(map(int, input().split()))
-# oil.pop()
-cnt = 0
-coin = 0
-i = 0 # 기름
-j = 0 # 길
-while j < len(road):
-    print(road[j])
-    if cnt < road[j]:
-        cnt += 1
-        coin += oil[i]
-        continue
-    if oil[i] < oil[i+1]:
-        cnt = 0
-        j += 1
-        continue
-    if oil[i] >= oil[i+1]:
-        cnt = 0
-        i += 1
-        j += 1
-        continue
-print(coin)
 
-
-# for i in range(len(road)-1):
-#
-#     while cnt < road[i]:
-#         cnt += 1
-#         coin += oil[i]
-
-
-
-
-
-
-# 2 3 1
-# 5 4 2 1
-
-# 30 - 20 + 16 - 4 + 2
-# 24
-# 30 - 20 + 8
-
-# 18
+tt = 0
+ans = 0
+while tt < n-1:
+    num = 0
+    for i in range(tt+1, n):
+        num += road[i-1]
+        if oil[i] <= oil[tt] or i == n-1:
+            ans += oil[tt] * num
+            tt = i
+            break
+print(ans)
