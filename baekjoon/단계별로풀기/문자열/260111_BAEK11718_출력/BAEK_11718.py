a2d7347e08fab9829ca8aeefd25cff6d2b986869
김영훈 <yhkor11@naver.com>
2026-01-11 16:15:03 +0900

    file upload - BAEK_11718

---
+++ baekjoon/단계별로풀기/문자열/260111_BAEK11718_출력/BAEK_11718.py
@@ -0,0 +1,12 @@
+# 1번 import sys 활용
+# import sys
+# print(sys.stdin.read(), end="")
+
+
+# 2번 try-catch 활용
+# while True:
+#     try:
+#         line = input()
+#         print(line)
+#     except EOFError:
+#         break
